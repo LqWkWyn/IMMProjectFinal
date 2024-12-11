5fab23e28abbbd7ccee3133dd655fc3e47e67b84
LqWkWyn <laurence.quinn.work@gmail.com>
2024-12-11 21:11:33 +0000

    ADDED MUTE CHECKBOX

---
+++ Assets/src/Scripts/MuteButton.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuteButton : MonoBehaviour
+{
+   public void MuteToggle(bool muted){
+    if(muted){
+        AudioListener.volume = 0;
+
+    }
+    else{
+        AudioListener.volume = 1;
+    }
+
+   }
+}
